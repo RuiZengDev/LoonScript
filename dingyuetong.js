9c7302b8588c215da8545d2257b3e4489b03fc8d
RuiZeng <132585299+RuiZengDev@users.noreply.github.com>
2024-01-02 02:04:00 +0800

    Update dingyuetong.js

--- dingyuetong.js
+++ dingyuetong.js
@@ -1,8 +1,5 @@
-#!name= 订阅通
-#!desc=
-
 [Script]
-订阅通= type=http-response,requires-body=1,max-size=0,pattern=^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$,script-path=https://raw.githubusercontent.com/Guding88/Script/main/dingyuetong.js
+http-response ^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ script-path=https://raw.githubusercontent.com/Guding88/Script/main/dingyuetong.js, requires-body=true, timeout=10, tag=订阅通
 
 [MITM]
-hostname = %APPEND% buy.itunes.apple.com
+hostname = buy.itunes.apple.com
